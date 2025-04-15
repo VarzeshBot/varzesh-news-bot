1bb57a1cbedb665c192f9804eea771c37f182580
VarzeshBot <efazatvahid@gmail.com>
2025-04-15 23:39:29 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ SEARCH_ENGINE_ID = "9171969b9d6eb4efa"
 
 # دریافت آخرین خبر از RSS
 def get_latest_news():
-    rss_url = "https://www.varzesh3.com/rss/all"
+    rss_url = "https://www.varzesh3.com/news/rss/all"
     response = requests.get(rss_url)
     soup = BeautifulSoup(response.content, features="xml")
     items = soup.findAll("item")
