1a09db83f625d8712026f7afe56370b1c74128e5
VarzeshBot <efazatvahid@gmail.com>
2025-04-16 00:10:21 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,20 @@
 import requests
-import json
 from bs4 import BeautifulSoup
 from deep_translator import GoogleTranslator
 from telegram import Bot
-
+import json
+import os
 
 # تنظیمات شما
-TOKEN = "8107821630:AAGYeDcX9u0gsuGRL0bscEtNullhjeo8cIQ"
-CHANNEL_USERNAME = "@akhbar_varzeshi_roz_iran"
-GOOGLE_API_KEY = "AIzaSyDyPE9Mk0JhL-wvzFEg1OGBqy6o8LjAaGc"
-SEARCH_ENGINE_ID = "9171969b9d6eb4efa"
-
-# دریافت آخرین خبر از RSS
+TOKEN = os.environ.get
+ "8107821630:AAGYeDcX9u0gsuGRL0bscEtNullhjeo8cIQ"
+CHANNEL_USERNAME = os.environ.get
+ "@akhbar_varzeshi_roz_iran"
+GOOGLE_API_KEY = os.environ.get
+ "AIzaSyDyPE9Mk0JhL-wvzFEg1OGBqy6o8LjAaGc"
+SEARCH_ENGINE_ID = os.environ.get
+ "9171969b9d6eb4efa"
+# دریافت آخرین خبر RSS
 def get_latest_news():
     rss_url = "https://www.varzesh3.com/news/rss/all"
     response = requests.get(rss_url)
@@ -24,11 +27,11 @@ def get_latest_news():
     link = article.link.text
     return title, link
 
-# ترجمه عنوان خبر
+# ترجمه تیتر
 def translate_title(title_fa):
     return GoogleTranslator(source='auto', target='en').translate(title_fa)
 
-# جستجوی عکس مرتبط
+# جستجوی عکس در گوگل
 def search_image(query):
     search_url = f"https://www.googleapis.com/customsearch/v1?q={query}&cx={SEARCH_ENGINE_ID}&key={GOOGLE_API_KEY}&searchType=image"
     response = requests.get(search_url).json()
@@ -50,7 +53,7 @@ def read_last_news():
     except:
         return {"last_title": "", "last_link": ""}
 
-# ارسال پیام به تلگرام
+# ارسال خبر به تلگرام
 def send_news():
     bot = Bot(token=TOKEN)
     news = get_latest_news()
@@ -60,21 +63,22 @@ def send_news():
         # بررسی تکراری نبودن
         last = read_last_news()
         if title_fa == last["last_title"] or link == last["last_link"]:
-            print("خبر تکراریه، ارسال نمی‌شود.")
+            print("خبر تکراری است، ارسال نمی‌شود.")
             return
 
         # ادامه فقط اگر خبر جدید بود
         title_en = translate_title(title_fa)
         image_url = search_image(title_en)
 
-        message = f"📣 اخبار ورزشی\n\n🏆 {title_fa}\n\n🔗 {link}\n\n@akhbar_varzeshi_roz_iran"
+        # پیام HTML حرفه‌ای
+        message = f"<b>📣 اخبار ورزشی</b>\n\n<b>{title_fa}</b>\n\n<a href='{link}'>مشاهده خبر</a>\n\n@akhbar_varzeshi_roz_iran"
 
         if image_url:
-            bot.send_photo(chat_id=CHANNEL_USERNAME, photo=image_url, caption=message)
+            bot.send_photo(chat_id=CHANNEL_USERNAME, photo=image_url, caption=message, parse_mode='HTML')
         else:
-            bot.send_message(chat_id=CHANNEL_USERNAME, text=message)
+            bot.send_message(chat_id=CHANNEL_USERNAME, text=message, parse_mode='HTML')
 
-        # ذخیره آخرین خبر
         write_last_news(title_fa, link)
 
+# اجرای اصلی
 send_news()
